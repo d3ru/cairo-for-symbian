fa734aec70cb5b8611ea5e9304df7de1fe7a66bb
iwanj@users.sourceforge.net <iwanj@users.sourceforge.net@a20834f0-d954-11dd-a2b5-e5f827957e07>
2009-02-01 14:44:04 +0000

    add udeb

--- hellocairo/sis/create_package.bat
+++ hellocairo/sis/create_package.bat
@@ -1,2 +1,10 @@
 @echo off
-createsis create -cert cert-gen.cer -key key-gen.key -pass abcd hellocairo_gcce.pkg
+if "%1"=="udeb" goto udeb
+
+call createsis create -cert cert-gen.cer -key key-gen.key -pass abcd hellocairo_gcce.pkg
+goto end
+
+:udeb
+createsis create -cert cert-gen.cer -key key-gen.key -pass abcd hellocairo_gcce_udeb.pkg
+
+:end
