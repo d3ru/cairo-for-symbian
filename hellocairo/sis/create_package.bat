f0eab0a94c286d1f46b473c2b80483b731eefde8
iwanj@users.sourceforge.net <iwanj@users.sourceforge.net@a20834f0-d954-11dd-a2b5-e5f827957e07>
2009-02-07 01:47:51 +0000

    add armv5

--- hellocairo/sis/create_package.bat
+++ hellocairo/sis/create_package.bat
@@ -1,7 +1,12 @@
 @echo off
 if "%1"=="udeb" goto udeb
+if "%1"=="armv5" goto armv5
 
 call createsis create -cert cert-gen.cer -key key-gen.key -pass abcd hellocairo_gcce.pkg
+goto end
+
+:armv5
+call createsis create -cert cert-gen.cer -key key-gen.key -pass abcd hellocairo_armv5.pkg
 goto end
 
 :udeb
